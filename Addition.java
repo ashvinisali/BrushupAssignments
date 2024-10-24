f13b286571844fe00ab23040d4d495b54f3864e3
73124_Ashvini <ashvinisali1605@gmail.com>
2024-10-25 00:22:04 +0530

    First Commit

---
+++ Addition.java
@@ -0,0 +1,10 @@
+package BrushupAssignments;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
